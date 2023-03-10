181917d87c60532e2d573811fa1e0372fca77b11
birrellc <vaiezmedia@gmail.com>
2023-03-10 11:37:27 +0000

    Feat: setup ComputersCanvas to display computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,19 +1,51 @@
-import { Suspense, useEffect, useState } from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
 const Computers = () => {
   // import gltf model
-  const computer = useGLTF('./desktop_pc_/scene.gltf');
+  const computer = useGLTF('./desktop_pc/scene.gltf');
 
-  return;
-  // three js elements use a mesh instead of a div
-
-  <mesh>
-    {/* light so we can see the model */}
-    {/* https://docs.pmnd.rs/react-three-fiber/getting-started/your-first-scene#adding-lights */}
-    <hemisphereLight intensity={0.15} groundColor={'black'} />
-    <pointLight intensity={1} />{' '}
-  </mesh>;
+  return (
+    // three js elements use a mesh instead of a div
+    <mesh>
+      {/* light so we can see the model */}
+      {/* https://docs.pmnd.rs/react-three-fiber/getting-started/your-first-scene#adding-lights */}
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      {/* responsive light on the computer model */}
+      <pointLight intensity={1} />
+      {/* primitive is where we pass the object */}
+      <primitive object={computer.scene} />
+    </mesh>
+  );
 };
 
-export default Computers;
+const ComputersCanvas = () => {
+  // three js model canvas
+  // https://docs.pmnd.rs/react-three-fiber/getting-started/your-first-scene#setting-up-the-canvas
+  // camera is the view of the model - x,y,z axis for position
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      // https://threejs.org/docs/#api/en/renderers/WebGLRenderer.preserveDrawingBuffer
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      {/* react feature to allow loader when model loads */}
+      <Suspense>
+        {/* controls to move the model left and right */}
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
