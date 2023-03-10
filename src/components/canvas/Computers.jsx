29dd4d1e6b1799d80337be768df342f32a5dd4d2
birrellc <vaiezmedia@gmail.com>
2023-03-10 10:37:14 +0000

    Feat: created base light for computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  // import gltf model
+  const computer = useGLTF('./desktop_pc_/scene.gltf');
 
-export default Computers
+  return;
+  // three js elements use a mesh instead of a div
+
+  <mesh>
+    {/* light so we can see the model */}
+    {/* https://docs.pmnd.rs/react-three-fiber/getting-started/your-first-scene#adding-lights */}
+    <hemisphereLight intensity={0.15} groundColor={'black'} />
+  </mesh>;
+};
+
+export default Computers;
